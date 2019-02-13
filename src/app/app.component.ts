cf4cf12f447fcdc37318ee18187070b60754353b
Savepong <savepong.com@gmail.com>
2019-02-13 11:34:52 +0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,16 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyAn1v8hyxFz6Pay8Oky8FZdhmWDZICQEog',
+  authDomain: 'savepong-ionic-bulletinboard.firebaseapp.com',
+  databaseURL: 'https://savepong-ionic-bulletinboard.firebaseio.com',
+  projectId: 'savepong-ionic-bulletinboard',
+  storageBucket: 'savepong-ionic-bulletinboard.appspot.com',
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
@@ -22,5 +32,6 @@ export class AppComponent {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+    firebase.initializeApp(config);
   }
 }
---
+++ src/app/edit/edit.page.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'Firebase';
+import { ActivatedRoute, Router  } from '@angular/router';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators, FormArray } from '@angular/forms';
+
+@Component({
+  selector: 'app-edit',
+  templateUrl: './edit.page.html',
+  styleUrls: ['./edit.page.scss'],
+})
+export class EditPage implements OnInit {
+
+  ref = firebase.database().ref('infos/');
+  infoForm: FormGroup;
+
+  constructor(private route: ActivatedRoute,
+    public router: Router,
+    private formBuilder: FormBuilder
+  ) {
+    this.infoForm = this.formBuilder.group({
+      'info_title' : [null, Validators.required],
+      'info_description' : [null, Validators.required]
+    });
+    this.getInfo(this.route.snapshot.paramMap.get('key'));
+  }
+
+  getInfo(key) {
+    firebase.database().ref('infos/' + key).on('value', resp => {
+      const info = snapshotToObject(resp);
+      this.infoForm.controls['info_title'].setValue(info.info_title);
+      this.infoForm.controls['info_description'].setValue(info.info_description);
+    });
+  }
+
+  updateInfo() {
+    const newInfo = firebase.database().ref('infos/' + this.route.snapshot.paramMap.get('key')).update(this.infoForm.value);
+    this.router.navigate(['/detail/' + this.route.snapshot.paramMap.get('key')]);
+  }
+
+  ngOnInit() {
+  }
+
+}
+
+export const snapshotToObject = snapshot => {
+  const item = snapshot.val();
+  item.key = snapshot.key;
+
+  return item;
+};
